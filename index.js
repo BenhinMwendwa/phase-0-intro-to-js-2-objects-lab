177a9cf8a78376d53529d6b271929e31742bacba
Benhin <benhinmwendwa7@gmail.com>
2024-03-30 14:25:43 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={
+    name :"benhin",
+    streetAddress :"90400 mwingi",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return{
+    ...employee,
+    [key]:value 
+}
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     
+    employee[key]=value
+    return employee
+
+}function deleteFromEmployeeByKey(employee, key){
+     const newEmployee={...employee}
+     delete newEmployee[key]
+     return newEmployee
+
+}
+function   destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee[key]
+return employee
+    
+}
